84b02b9fb9cfcb5466b8731bb0d3d24446621104
Abdel Zer <abdelkader.zerouali@gmail.com>
2019-02-19 14:02:34 +0100

    add comment

--- PL1_Q22.py
+++ PL1_Q22.py
@@ -1,5 +1,6 @@
 from optlang import Model, Variable, Constraint, Objective
 
+''' valeurs des f^a  à f^l, à récupérer depuis une liste et utilisé pour la question 2.2'''
 y1 = 17
 y2 = 14.5
 y3 = y4 = y5 = y6 = 12.5
