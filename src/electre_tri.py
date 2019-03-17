8177f1823e09dab9791a6f6972cc74695d59baf3
IkramBouhya <ikrambouhya@gmail.com>
2019-03-17 17:13:29 +0100

    Electre-tri final

--- src/electre_tri.py
+++ src/electre_tri.py
@@ -1,7 +1,15 @@
 from loader import loadModel, parseDataframe
 import numpy as np
-    
+
+
 def comparesTo(a,b): # cette fonction retourne "True" quand a >= b sinon elle retourne "False"
+    """
+    Compare deux notes a et b 
+    
+    Args : notes a et b que nous souhaitons comparer
+    Return : un booléen valant True/1 si a est supérieur à b sinon il retourne False/0
+    
+    """
     comparator=0
     if(a[0]=="+" and b[0]=="+" and len(a)>=len(b)):
         comparator=1
@@ -15,8 +23,18 @@ def comparesTo(a,b): # cette fonction retourne "True" quand a >= b sinon elle re
     return comparator
 
  
-# Concordance partielle (H,bi)
 def ConcordancePartielleHbi(csv_name,direction,type='excel'):
+    """
+    Calcule la matrice de concordance partielle entre les produits et les profils selon le nombre de critères
+    
+    Args : 
+        csv_name: le nom du fichier de données placé dans le répertoire data
+        type: le type de fichier ('excel' ou 'csv')
+        direction: si on veut maximiser ou minimiser 
+        
+    Return : la matrice de concordance partielle (H,bi)
+    
+    """
     df = loadModel(csv_name, type)
     _ , _, df_criteria_list, _, _, df_criteria_profils, _, _ = parseDataframe(df)  
     nb_criteria=len(df_criteria_list.columns) #nb de critères
@@ -36,7 +54,6 @@ def ConcordancePartielleHbi(csv_name,direction,type='excel'):
                  for j in range(0,nb_profils):
                     concMatrixHbi[i,j]= comparesTo(criteria.values[i,0], profilCriteria.values[j,0])
 
-            #print("conc",concMatrixHbi)
             listHbi.append(concMatrixHbi)
       
     elif(direction=="min"):
@@ -52,8 +69,18 @@ def ConcordancePartielleHbi(csv_name,direction,type='excel'):
           
     return listHbi
                 
-# Concordance partielle (bi,H)
 def ConcordancePartiellebiH(csv_name,direction,type='excel'):
+    """
+    Calcule la matrice de concordance partielle entre les profils et les produits par nombre de critères
+    
+    Args : 
+        csv_name: le nom du fichier de données placé dans le répertoire data
+        type: le type de fichier ('excel' ou 'csv')
+        direction: si on veut maximiser ou minimiser 
+        
+    Return : la matrice de concordance partielle (bi,H)
+    
+    """
     df = loadModel(csv_name, type)
     _ , _, df_criteria_list, _, _, df_criteria_profils, _, _ = parseDataframe(df)  
     nb_criteria=len(df_criteria_list.columns) #nb de critères
@@ -88,8 +115,18 @@ def ConcordancePartiellebiH(csv_name,direction,type='excel'):
     return listbiH              
                 
                 
-#Matrice de concordance globale (H,bi)                
 def ConcordonceGlobaleHbi(csv_name,direction,type='excel'):
+    """
+    Calcule la matrice de concordance globale entre les profils et les produits 
+    
+    Args : 
+        csv_name: le nom du fichier de données placé dans le répertoire data
+        type: le type de fichier ('excel' ou 'csv')
+        direction: si on veut maximiser ou minimiser 
+        
+    Return : la matrice de concordance globale (bi,H)
+    
+    """
     df = loadModel(csv_name, type)
     _ ,coeff_list, df_criteria_list, _, _, df_criteria_profils, _, _ = parseDataframe(df) 
     nb_criteria=len(df_criteria_list.columns)-1 #nb de critères
@@ -102,19 +139,27 @@ def ConcordonceGlobaleHbi(csv_name,direction,type='excel'):
     
     for z in range(nb_criteria):
         sum_weight+=coeff_list[z]
-        #print("somme",sum_weight) 
     
     for i in range(0,nb_produits):
         for j in range(0,nb_profils): 
             for k in range(0,nb_criteria):
                 globalMatrixHbi[i,j] += (coeff_list[k]*listHbi[k][i][j])/sum_weight
-                #print(coeff_list[k],i,j,listHbi[k][i][j],globalMatrixHbi[i,j])
-
                 
     return globalMatrixHbi
     
-#Matrice de concordance globale (bi,H)                
+
 def ConcordonceGlobalebiH(csv_name,direction,type='excel'):
+    """
+    Calcule la matrice de concordance globale entre les profils et les produits
+    
+    Args : 
+        csv_name: le nom du fichier de données placé dans le répertoire data
+        type: le type de fichier ('excel' ou 'csv')
+        direction: si on veut maximiser ou minimiser 
+        
+    Return : la matrice de concordance globale (bi,H)
+    
+    """
     df = loadModel(csv_name, type)
     _ ,coeff_list, df_criteria_list, _, _, df_criteria_profils, _, _ = parseDataframe(df) 
     nb_criteria=len(df_criteria_list.columns)-1 #nb de critères
@@ -137,6 +182,18 @@ def ConcordonceGlobalebiH(csv_name,direction,type='excel'):
 
 
 def SurclassementHbi(lamda,csv_name,direction,type='excel'):
+    """
+    Calcule la matrice de surclassement entre les produits et les profils
+    
+    Args : 
+        lamda : le seuile de majorité
+        csv_name: le nom du fichier de données placé dans le répertoire data
+        type: le type de fichier ('excel' ou 'csv')
+        direction: si on veut maximiser ou minimiser 
+        
+    Return : la matrice de surclassement (H,bi)
+    
+    """
     df = loadModel(csv_name, type)
     _ ,_, df_criteria_list, _, _, df_criteria_profils, _, _ = parseDataframe(df) 
     nb_produits=len(df_criteria_list) #nombre de produits
@@ -152,6 +209,18 @@ def SurclassementHbi(lamda,csv_name,direction,type='excel'):
     return SurcHbi
 
 def SurclassementbiH(lamda,csv_name,direction,type='excel'):
+    """
+    Calcule la matrice de surclassement entre les profils et les produits
+    
+    Args : 
+        lamda : le seuile de majorité
+        csv_name: le nom du fichier de données placé dans le répertoire data
+        type: le type de fichier ('excel' ou 'csv')
+        direction: si on veut maximiser ou minimiser 
+        
+    Return : la matrice de surclassement (bi,H)
+    
+    """
     df = loadModel(csv_name, type)
     _ ,_, df_criteria_list, _, _, df_criteria_profils, _, _ = parseDataframe(df) 
     nb_produits=len(df_criteria_list) #nombre de produits
@@ -168,6 +237,18 @@ def SurclassementbiH(lamda,csv_name,direction,type='excel'):
 
              
 def EvaluationPessimiste(lamda,csv_name,direction,type='excel'):
+    """
+    Applique l'évaluation pessimiste sur les matrices de surclassement
+    
+    Args : 
+        lamda : le seuil de majorité 
+        csv_name: le nom du fichier de données placé dans le répertoire data
+        type: le type de fichier ('excel' ou 'csv')
+        direction: si on veut maximiser ou minimiser 
+        
+    Return : une liste d'affecation de catégories aux données 
+    
+    """
     df = loadModel(csv_name, type)
     _ ,_, df_criteria_list, _, _, df_criteria_profils, _, categories = parseDataframe(df) 
     nb_produits=len(df_criteria_list) #nombre de produits
@@ -190,6 +271,18 @@ def EvaluationPessimiste(lamda,csv_name,direction,type='excel'):
     return AffectationPessimiste
 
 def EvaluationOptimiste(lamda,csv_name,direction,type='excel'):
+    """
+    Applique l'évaluation optimiste sur les matrices de surclassement
+    
+    Args : 
+        lamda : le seuil de majorité 
+        csv_name: le nom du fichier de données placé dans le répertoire data
+        type: le type de fichier ('excel' ou 'csv')
+        direction: si on veut maximiser ou minimiser 
+        
+    Return : une liste d'affecation de catégories aux données 
+    
+    """
     df = loadModel(csv_name, type)
     _ ,_, df_criteria_list, _, _, df_criteria_profils, _, categories = parseDataframe(df) 
     nb_produits=len(df_criteria_list) #nombre de produits
@@ -204,13 +297,26 @@ def EvaluationOptimiste(lamda,csv_name,direction,type='excel'):
             if SurcbiH[5-j,i]==0:
                 continue
             elif SurcbiH[5-j,i]==1 and SurcHbi[i,5-j]==0:
-                AffectationOptimiste.append(categories.get("C"+str(6-j)))
+                AffectationOptimiste.append(categories.get("C"+str(j)))
                 break
             
     return AffectationOptimiste
 
     
 def compareClassification(TypeEval,lamda,csv_name,direction,type='excel'):
+    """
+    compare la méthode d'évaluation utilisé avec les résultats obtenues par le magazine
+    
+    Args : 
+        TypeEval : le type d'évaluation utilisé Optimiste ou Pessimiste
+        lamda : le seuil de majorité 
+        csv_name: le nom du fichier de données placé dans le répertoire data
+        type: le type de fichier ('excel' ou 'csv')
+        direction: si on veut maximiser ou minimiser 
+        
+    Return : un seuil de mauvaise classification
+    
+    """
     global IMauvC
     AffectationOptimiste = EvaluationOptimiste(lamda,csv_name,direction,type='excel')
     AffectationPessimiste = EvaluationPessimiste(lamda,csv_name,direction,type='excel')
