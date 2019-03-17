9e5a9c0c83349acb6e053c3633bb1d19b61ecbae
IkramBouhya <ikrambouhya@gmail.com>
2019-03-17 17:14:10 +0100

    Electre-tri final

--- src/main_electre_tri_couches.py
+++ src/main_electre_tri_couches.py
@@ -9,9 +9,9 @@
 """
 
 
-from ressources.electre_tri import ConcordancePartielleHbi, ConcordancePartiellebiH, ConcordonceGlobaleHbi,ConcordonceGlobalebiH 
-from ressources.electre_tri import SurclassementHbi,SurclassementbiH,EvaluationPessimiste,EvaluationOptimiste, compareClassification
-from ressources.loader import getElectreTriData, exportInExcel
+from electre_tri import ConcordancePartielleHbi, ConcordancePartiellebiH, ConcordonceGlobaleHbi,ConcordonceGlobalebiH 
+from electre_tri import SurclassementHbi,SurclassementbiH,EvaluationPessimiste,EvaluationOptimiste, compareClassification
+from loader import getElectreTriData, exportInExcel
 import pandas as pd
 
 def main():
@@ -34,12 +34,19 @@ def main():
    print("Concordance partielle (bi,H)", ConcordancePartiellebiH(csv_name=csv_name,
                       direction=direction))
 
+#------------------------------------------------------------
+#Etape 2: Concordance globale
+#------------------------------------------------------------
+   
    print("Concordance globale (H,bi)", ConcordonceGlobaleHbi(csv_name=csv_name,
                       direction=direction ))
 
    print("Concordance globale (bi,H)", ConcordonceGlobalebiH(csv_name=csv_name,
                       direction=direction ))
    
+#------------------------------------------------------------
+#Etape 3-1: Matrices de surclassement pour Lambda = 0.55 
+#------------------------------------------------------------
    
    print('---------------------------------------------------------------')
    print('lambda = 0.55')
@@ -54,23 +61,28 @@ def main():
                      csv_name=csv_name,
                      direction=direction))
    
+#------------------------------------------------------------------------------
+#Etape 4-1: Application de l'Evaluation Optimiste & Pessimiste pour lamda=0.55
+#------------------------------------------------------------------------------  
+    
    
-   
-   
-   
-   EvalOptimisteResult_lambda_1 = EvaluationPessimiste(lamda=lambda_1,
+   EvalPessimisteResult_lambda_1 = EvaluationPessimiste(lamda=lambda_1,
                      csv_name=csv_name,
                      direction=direction)
    
-   print("Evaluation Pessimiste - lambda 0.55", EvalOptimisteResult_lambda_1)
+   print("Evaluation Pessimiste - lambda 0.55", EvalPessimisteResult_lambda_1)
    
    
-   EvalPessimisteResult_lambda_1 = EvaluationOptimiste(lamda=lambda_1,
+   EvalOptimisteResult_lambda_1 = EvaluationOptimiste(lamda=lambda_1,
                      csv_name=csv_name,
                      direction=direction)
    
-   print("Evaluation Pessimiste - lambda 0.55 ",EvalPessimisteResult_lambda_1 )
+   print("Evaluation Optimiste - lambda 0.55 ",EvalOptimisteResult_lambda_1 )
    
+
+#----------------------------------------------------------------------
+#Etape 5-1: Calcul des taux de mauvaise classification pour lamda=0.55
+#----------------------------------------------------------------------
    
    taux_mauvaise_classification_opt_lambda_1= compareClassification(TypeEval="optimiste",
                                                        lamda=lambda_1,
@@ -97,6 +109,10 @@ def main():
    new_df_lambda1 = pd.concat([result_original, df_Eval_Opt_lambda1, df_Eval_Pes_lambda1], axis=1, sort=False)
    
    result_lambda1 = {"Tx Optimiste": taux_mauvaise_classification_opt_lambda_1, "Tx Pessimiste" :taux_mauvaise_classification1_pess_lambda_1  }
+ 
+#------------------------------------------------------------
+#Etape 3-2: Matrices de surclassement pour Lambda = 0.75 
+#------------------------------------------------------------ 
    
    print('---------------------------------------------------------------')
    print('lambda = 0.75')
@@ -110,31 +126,35 @@ def main():
    print("Matrice de Surclassement (bi,H) - lambda 0.55", SurclassementbiH(lamda=lambda_2,
                      csv_name=csv_name,
                      direction=direction))
+
+#-----------------------------------------------------------------------------
+#Etape 4-2: Application de l'Evaluation Optimiste & Pessimiste pour lamda=0.75
+#-----------------------------------------------------------------------------    
    
    
-   
-   
-   
-   EvalOptimisteResult_lambda_2 = EvaluationPessimiste(lamda=lambda_2,
+   EvalPessimisteResult_lambda_2 = EvaluationPessimiste(lamda=lambda_2,
                      csv_name=csv_name,
                      direction=direction)
    
-   print("Evaluation Pessimiste - lambda 0.55", EvalOptimisteResult_lambda_2)
+   print("Evaluation Pessimiste - lambda 0.75", EvalPessimisteResult_lambda_2)
    
    
-   EvalPessimisteResult_lambda_2 = EvaluationOptimiste(lamda=lambda_2,
+   EvalOptimisteResult_lambda_2 = EvaluationOptimiste(lamda=lambda_2,
                      csv_name=csv_name,
                      direction=direction)
    
-   print("Evaluation Pessimiste - lambda 0.55 ",EvalPessimisteResult_lambda_2 )
+   print("Evaluation Optimiste - lambda 0.75 ",EvalOptimisteResult_lambda_2 )
    
+#----------------------------------------------------------------------
+#Etape 5-2: Calcul des taux de mauvaise classification pour lamda=0.75
+#----------------------------------------------------------------------
    
    taux_mauvaise_classification_opt_lambda_2= compareClassification(TypeEval="optimiste",
                                                        lamda=lambda_2,
                                                        csv_name=csv_name,
                                                        direction=direction)
    
-   print("Taux mauvaise classification Opt - lambda 0.55", taux_mauvaise_classification_opt_lambda_2)
+   print("Taux mauvaise classification Opt - lambda 0.75", taux_mauvaise_classification_opt_lambda_2)
    
    
    taux_mauvaise_classification1_pess_lambda_2 = compareClassification(TypeEval="pessimiste",
@@ -142,7 +162,7 @@ def main():
                                                        csv_name=csv_name,
                                                        direction=direction)
    
-   print("Taux mauvaise classification Pess - lambda 0.55", taux_mauvaise_classification1_pess_lambda_2)
+   print("Taux mauvaise classification Pess - lambda 0.75", taux_mauvaise_classification1_pess_lambda_2)
    
    
    
